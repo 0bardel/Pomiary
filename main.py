c8062d03661b175d7ea436fc1e60216be87e84da
negative-seven <21268649+negative-seven@users.noreply.github.com>
2023-12-05 10:13:16 +0100

    Add --dummy-data flag for program use without raspberry pi i/o

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-#importy
+import argparse
 import time
-import adafruit_adxl34x
-import board
-import busio
 import math
-import queue
-from threading import Thread
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import os
-import RPi.GPIO as GPIO
 from time import sleep
 from multiprocessing import Process, Value
 
-GPIO.setmode(GPIO.BCM)
-
-dirX = 19
-stepX = 26
-
-dirY = 21
-stepY = 20
-
-GPIO.setup(19,GPIO.OUT) # dir
-GPIO.setup(26,GPIO.OUT) # step
-GPIO.setup(21,GPIO.OUT) # dir
-GPIO.setup(20,GPIO.OUT) # step
-
-
-i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
-#klasa ogarniajaca silniki krokowe
 
 def step(angle, directionPin, stepPin):
     direction = 0
-    
+
     while True:
         direction = int(angle.value / 2.0)
         if direction > 0:
@@ -47,23 +23,15 @@ def step(angle, directionPin, stepPin):
                 GPIO.output(stepPin, i % 2)
                 sleep(sleepTime)
 
-xAngle = Value("i", 0)
-yAngle = Value("i", 0)
-xPrevious = Value("i", 0)
-yPrevious = Value("i", 0)
-x = Process(target=step, args=(xAngle, dirX, stepX))
-y = Process(target=step, args=(yAngle, dirY, stepY))
-x.start()
-y.start()
 
 def anim(xAngle, yAngle):
-    
-    fig, axs = plt.subplots(1, 2,figsize=(12,6))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
     fig.suptitle('Gimbal')
     xs = []
     ys = []
     t = []
-    def animate(i, xs, ys,t, xAngle, yAngle):
+
+    def animate(i, xs, ys, t, xAngle, yAngle):
         xs.append(xAngle.value)
         ys.append(yAngle.value)
         t.append(time.time())
@@ -75,8 +43,8 @@ def anim(xAngle, yAngle):
         axs[0].scatter(xs, ys)
 
         # Format plot
-        plt.setp(axs[0],xlim = (-25,25))
-        fig[0].ylim(-25,25)
+        plt.setp(axs[0], xlim=(-25, 25))
+        fig[0].ylim(-25, 25)
         plt.title('Angle over time')
         plt.xlabel("Kąt X")
         plt.ylabel("Kąt Y")
@@ -85,32 +53,80 @@ def anim(xAngle, yAngle):
         fig[1].plot(t, ys)
         plt.ion()
         return plt.plot()
-    aaa = animation.FuncAnimation(fig, animate, fargs=(xs, ys,t, xAngle, yAngle), interval=500, cache_frame_data=False)
+    aaa = animation.FuncAnimation(fig, animate, fargs=(
+        xs, ys, t, xAngle, yAngle), interval=500, cache_frame_data=False)
     plt.show()
 
-a = Process(target=anim, args=(xAngle, yAngle))
-a.start()
 
+def main():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('--dummy-data', action='store_true')
+    dummy_data = argument_parser.parse_args().dummy_data
 
+    if not dummy_data:
+        import adafruit_adxl34x
+        import board
+        import busio
+        import RPi.GPIO as GPIO
 
-alphaHist=[]
-betaHist=[]
+        GPIO.setmode(GPIO.BCM)
 
-prevTime = time.time()
-while True:
-    xaccel = accelerometer.acceleration[0]
-    yaccel = accelerometer.acceleration[1]
-    zaccel = accelerometer.acceleration[2]
+        GPIO.setup(19, GPIO.OUT)  # dir
+        GPIO.setup(26, GPIO.OUT)  # step
+        GPIO.setup(21, GPIO.OUT)  # dir
+        GPIO.setup(20, GPIO.OUT)  # step
 
-    alpha = math.atan2(zaccel, yaccel) * 360 / (2 * math.pi) + 90
-    beta = math.atan2(zaccel, xaccel) * 360 / (2 * math.pi) + 90
-    xAngle.value = int(alpha)
-    yAngle.value = int(beta)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
-    time.sleep(0.02)
-    print(time.time() - prevTime)
+    dirX = 19
+    stepX = 26
+
+    dirY = 21
+    stepY = 20
+
+    # klasa ogarniajaca silniki krokowe
+
+    xAngle = Value("i", 0)
+    yAngle = Value("i", 0)
+    xPrevious = Value("i", 0)
+    yPrevious = Value("i", 0)
+
+    if not dummy_data:
+        x = Process(target=step, args=(xAngle, dirX, stepX))
+        y = Process(target=step, args=(yAngle, dirY, stepY))
+        x.start()
+        y.start()
+
+    a = Process(target=anim, args=(xAngle, yAngle))
+    a.start()
+
+    alphaHist = []
+    betaHist = []
+
     prevTime = time.time()
+    while True:
+        if dummy_data:
+            xaccel = math.sin(time.time())
+            yaccel = math.sin(time.time() * 1.1)
+            zaccel = math.sin(time.time() * 1.2)
+        else:
+            xaccel = accelerometer.acceleration[0]
+            yaccel = accelerometer.acceleration[1]
+            zaccel = accelerometer.acceleration[2]
+
+        alpha = math.atan2(zaccel, yaccel) * 360 / (2 * math.pi) + 90
+        beta = math.atan2(zaccel, xaccel) * 360 / (2 * math.pi) + 90
+        xAngle.value = int(alpha)
+        yAngle.value = int(beta)
+
+        time.sleep(0.02)
+        # print(time.time() - prevTime)
+        prevTime = time.time()
+
+        alphaHist.append(alpha)
+        betaHist.append(beta)
 
 
-    alphaHist.append(alpha)
-    betaHist.append(beta)
+if __name__ == '__main__':
+    main()
